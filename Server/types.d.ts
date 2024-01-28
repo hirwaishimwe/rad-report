0f78abf7841bdec08373bbb1a3ced8c29dc733a4
christopheralphonse <christopheralphonse96@gmail.com>
2024-01-27 23:15:18 -0500

    server up

---
+++ Server/types.d.ts
@@ -0,0 +1,8 @@
+type ConnectionOptionsExtend = {
+  useNewUrlParser: boolean;
+  useUnifiedTopology: boolean;
+  useCreateIndex: boolean;
+  useFindAndModify: boolean;
+  user?: string;
+  pass?: string;
+};
