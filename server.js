eef85403fa935dd0cb7504af1e69c7d6508b9c30
Dimitri <dimitri.legaspi@gmail.com>
2024-02-02 18:00:25 -0500

    Routed bad requests

--- server.js
+++ server.js
@@ -7,4 +7,15 @@ app.use('/', express.static(path.join(__dirname, '/client')))
 
 app.use('/', require('./routes/root'))
 
+app.all('*', (req, res) => {
+    res.status(404)
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'))
+    } else if (req.accepts('json')) {
+        res.json({ message: '404 Not Found'})
+    } else {
+        res.type('txt').send('404 Not Found')
+    }
+})
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
