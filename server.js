0d626f348618929f8bc1820de82e2492f531760a
Dimitri <dimitri.legaspi@gmail.com>
2024-02-02 17:55:00 -0500

    Created a node and express server

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const PORT = process.env.PORT || 3500
+
+app.use('/', express.static(path.join(__dirname, '/client')))
+
+app.use('/', require('./routes/root'))
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
