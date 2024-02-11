de1cb2975746f3fc596089d48e77d13b90437b44
Hirwa Ishimwe <118475346+hirwaishimwe@users.noreply.github.com>
2024-02-11 00:15:03 -0500

    update variables

--- client/src/pages/PatientDetails.js
+++ client/src/pages/PatientDetails.js
@@ -7,8 +7,8 @@ import './PatientDetails.css'
 function PatientDetails() {
     const { patientId } = useParams();
     const { examsData } = useContext(ExamContext);
-    const exam = examsData.find(exam => exam.patientId === patientId);
-    const patientExams = examsData.filter(exam => exam && exam.patientId === patientId);
+    const exam = examsData.find(exam => exam.medical_record_number === patientId);
+    const patientExams = examsData.filter(exam => exam && exam.medical_record_number === patientId);
 
     if (patientExams.length === 0) {
         return <div>Patient Exams not found</div>;
@@ -19,7 +19,7 @@ function PatientDetails() {
             <div className="info-section patient-info">
                 <h2>Patient Info</h2>
                 <div class="container">
-                    <p class="inline"><strong>Patient ID:</strong> {exam.patientId}</p>
+                    <p class="inline"><strong>Patient ID:</strong> {exam.medical_record_number}</p>
                     <p class="inline"><strong>Age:</strong> {exam.age}</p>
                     <p class="inline"><strong>Sex:</strong> {exam.sex}</p>
                     <p class="inline"><strong>BMI:</strong> {exam.bmi}</p>
