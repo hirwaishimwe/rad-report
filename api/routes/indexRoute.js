c35bc201cf64e1974e855df909d0e20a4e78e2a8
Jerison-dev <feliz1015@gmail.com>
2024-02-13 23:39:34 -0500

    changes

---
+++ api/routes/indexRoute.js
@@ -0,0 +1,23 @@
+import {
+    PatchUserById,
+    createUser,
+    deleteUserById,
+    getAllUsers,
+    getUserById,
+    updateUserById,
+} from "../controller/userController.js";
+
+import express from "express";
+import getApiStatus from "../controller/indexController.js";
+
+const router = express.Router();
+
+router.get("/api", getApiStatus);
+router.post("/api/users", createUser);
+router.get("/api/users", getAllUsers);
+router.get("/api/users/:userId", getUserById);
+router.put("/api/users/:userId", updateUserById);
+router.patch("/api/users/:userId", PatchUserById);
+router.delete("/api/users/:userId", deleteUserById);
+
+export default router;
