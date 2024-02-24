7e1ba78f9480b2b70a9876a83f621b6e8b36bcc3
christopheralphonse <christopheralphonse96@gmail.com>
2024-02-23 22:31:12 -0500

    update

--- client/src/pages/examTablePage/Pagination.jsx
+++ client/src/pages/examTablePage/Pagination.jsx
@@ -1,18 +1,46 @@
 import React from "react";
 
 export const Pagination = ({
-    activePage,
-    count,
-    rowsPerPage,
-    totalPages,
-    setActivePage,
+  activePage,
+  count,
+  rowsPerPage,
+  totalPages,
+  setActivePage,
 }) => {
-    return (
-        <div className="pagination">
-            <button>⏮️ First</button>
-            <button>⬅️ Previous</button>
-            <button>Next ➡️</button>
-            <button>Last ⏭️</button>
-        </div>
-    );
+  const beginning = activePage === 1 ? 1 : rowsPerPage * (activePage - 1) + 1;
+  const end = activePage === totalPages ? count : beginning + rowsPerPage - 1;
+
+  return (
+    <>
+      <div className="pagination">
+        <button disabled={activePage === 1} onClick={() => setActivePage(1)}>
+          ⏮️ First
+        </button>
+        <button
+          disabled={activePage === 1}
+          onClick={() => setActivePage(activePage - 1)}
+        >
+          ⬅️ Previous
+        </button>
+        <button
+          disabled={activePage === totalPages}
+          onClick={() => setActivePage(activePage + 1)}
+        >
+          Next ➡️
+        </button>
+        <button
+          disabled={activePage === totalPages}
+          onClick={() => setActivePage(totalPages)}
+        >
+          Last ⏭️
+        </button>
+      </div>
+      <p>
+        Page {activePage} of {totalPages}
+      </p>
+      <p>
+        Rows: {beginning === end ? end : `${beginning} - ${end}`} of {count}
+      </p>
+    </>
+  );
 };
